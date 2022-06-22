f8544ec5c84a098da3f394afc08cb87f8d240586
Marco Spieß <marco.spiess@constaff.com>
2022-06-22 11:03:42 +0200

    update config to run test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 const config = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  transform: {
+    "\\.[jt]sx?$": "ts-jest",
+  },
+  transformIgnorePatterns: ["/node_modules/(?!(uuid)/)"],
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/mocks/fileMock.ts",
+  },
 };
 module.exports = config;
